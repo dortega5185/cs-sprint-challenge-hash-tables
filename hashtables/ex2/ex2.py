ca51fcaeab8a8c00659b27e918b7e8c8b21b9ca5
David Mizativan-Ortega <dortega5185@gmail.com>
2020-07-10 14:18:48 -0500

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,26 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # prepopulate an array with the same number elements as tickets
+    destinations = [None] * length
 
-    return route
+    destination_lookup = dict()
+
+    # go through each ticket and store its immediate destination
+    for i in tickets:
+        destination_lookup[i.source] = i.destination
+    
+    # the starting ticket has a source of "NONE". Start there to build a chain
+    next_destination = destination_lookup["NONE"]
+
+    # look up each ticket's next destination and add it to the destinations array
+    # stop when the next destination is actually the string "NONE".
+    for current_leg in range(0, length):
+
+        # record next destination
+        destinations[current_leg] = next_destination        
+
+        # retrieve next destination
+        next_destination = destination_lookup[next_destination]
+
+    return destinations
